5b870575d2db9e4f3fc24f174798b26f517c6a78
chang ming-jui <changming-jui@changs-Mac.local>
2017-03-30 22:48:42 +0800

    commit

--- FirstHW/ViewController.swift
+++ FirstHW/ViewController.swift
@@ -1,9 +1,9 @@
 //
 //  ViewController.swift
-//  FirstHW
+//  RING
 //
-//  Created by chang ming-jui on 04/03/2017.
-//  Copyright © 2017 chang ming-jui. All rights reserved.
+//  Created by chang ming-jui on 2017/3/30.
+//  Copyright © 2017年 chang ming-jui. All rights reserved.
 //
 
 import UIKit
@@ -12,14 +12,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
---
+++ RingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RingViewController.swift
+//  RING
+//
+//  Created by chang ming-jui on 2017/3/30.
+//  Copyright © 2017年 chang ming-jui. All rights reserved.
+//
+
+import UIKit
+
+class RingViewController: NSObject {
+
+}
